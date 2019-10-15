9df2c1aebb0706477281e23ff913965ec90d22bf
Nick <nicholasmahe@gmail.com>
2019-10-15 14:39:18 -0600

    Initial Commit

---
+++ Project 2 Testing 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 2 Testing 2
+//
+//  Created by Nick Mahe on 10/15/19.
+//  Copyright © 2019 Nick Mahe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
